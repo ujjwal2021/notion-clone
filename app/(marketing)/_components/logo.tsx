11d23c6af70252afbc1487ea85f8c4f81275b396
Ujjwal Bhattarai <bhattaraiujjwal26@gmail.com>
2023-10-14 22:31:22 +0530

    init

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "600"]
+})
+
+
+export const Logo = () => {
+    return (
+        <div className="hidden md:flex items-center gap-x-2">
+            <Image
+                src="/logo.svg"
+                alt="logo"
+                height="40"
+                width="40"
+                className="dark:hidden"
+            />
+            <Image
+                src="/logo-dark.svg"
+                alt="logo"
+                height="40"
+                width="40"
+                className="hidden dark:block"
+            />
+            <p className={cn("font-semibold", font.className)}>Jotion</p>
+        </div>
+    )
+}
